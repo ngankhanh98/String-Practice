85f43d31e283b32aa8cd97b96c813996626c1636
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2018-11-11 21:41:14 +0700

    Feature completed

--- String-Practice/MainWindow.xaml.cs
+++ String-Practice/MainWindow.xaml.cs
@@ -24,5 +24,76 @@ namespace String_Practice
         {
             InitializeComponent();
         }
+
+        private void btnStats_Click(object sender, RoutedEventArgs e)
+        {
+            string stack = txtInput.Text;
+            const string Space = " ";
+            var tokens = stack.Split(new string[] { Space }, StringSplitOptions.None).Select(token => token.Trim().ToLower());
+
+            var dictionary = new Dictionary<string, int>();
+
+            foreach(var token in tokens)
+            {
+                if(dictionary.ContainsKey(token))
+                {
+                    dictionary[token]++;
+                }
+                else
+                {
+                    dictionary[token] = 1;
+                }
+            }
+
+            var screen = new Stats(dictionary);
+            screen.ShowDialog();
+        }
+
+        private void btnNor_Click(object sender, RoutedEventArgs e)
+        {
+            var fullname = txtInput.Text;
+            const string Space = " ";
+            var tokens = fullname.Split(new string[] { Space },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(token =>
+                {
+                    token = token.Trim().ToLower();
+                    var firstChar = token.Substring(0, 1).ToUpper();
+                    var remaining = token.Substring(1, token.Length - 1);
+                    return firstChar + remaining;
+                });
+
+            var builder = new StringBuilder();
+            foreach (var token in tokens)
+            {
+                builder.Append(token);
+                builder.Append(Space);
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+
+            string normalize = builder.ToString();
+            var screen = new Norm(normalize);
+            screen.ShowDialog();
+
+        }
+
+        private void btnFPInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtInput.Text;
+            const string BackSlash = @"\";
+            const string Dot = ".";
+            var foundPos = path.LastIndexOf(BackSlash);
+            var directory = path.Substring(0, foundPos);
+            var filename = path.Substring(foundPos + 1, path.Length - foundPos - 1);
+
+            foundPos = path.LastIndexOf(Dot);
+            var extension = path.Substring(foundPos + 1, path.Length - foundPos - 1);
+            string pathInfo = directory + " - " + filename + " - " + extension;
+
+            var screen = new Info(pathInfo);
+            screen.ShowDialog();
+
+        }
     }
 }
---
+++ String-Practice/Stats.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace String_Practice
+{
+    /// <summary>
+    /// Interaction logic for Stats.xaml
+    /// </summary>
+    public partial class Stats : Window
+    {
+        public Stats(Dictionary<string, int>dic)
+        {
+            InitializeComponent();
+            string output = "";
+
+            foreach(var entry in dic)
+            {
+                output += ((entry.Key) + " : " + entry.Value.ToString() + "\n");
+            }
+
+            lblStats.Content = output;
+        }
+    }
+}
